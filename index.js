e2d5812c7287bc1eda8ab08099b870f069aac2c9
pacotedemusicas <contato@pacotedemusicas.com>
2024-06-17 22:11:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-        window.location.href = "../teladelogin/pages/acesso.html";
+        window.location.href = "./teladelogin/pages/acesso.html";
     }
 })
 
@@ -20,7 +20,7 @@ function login() {
         form.email().value, form.password().value
     ).then(response => {
         hideLoading();
-        window.location.href = "../teladelogin/pages/acesso.html";
+        window.location.href = "./teladelogin/pages/acesso.html";
     }).catch(error => {
         hideLoading();
         alert(getErrorMessage(error));
