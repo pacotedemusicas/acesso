ad305014d0462faa1a5b411ea027e7660c6d4bb4
pacotedemusicas <contato@pacotedemusicas.com>
2024-06-17 21:51:31 -0300

    Delete pages directory

--- pages/script3.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('button-caminhoneiro').addEventListener('click', function() {
-    window.open('https://drive.google.com/drive/folders/162fBQaSBNWBrJMnHQtO27THux4XvHFXY', '_blank');
-});
--- pages/script8.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('videoButton').addEventListener('click', function() {
-    window.open('https://drive.google.com/drive/folders/1mHLkXMu_5i85agsZzpgtNDdWyplHEdm2', '_blank');
-});
