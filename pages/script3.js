ef9ec8767943583d307116d7bed3a4b49a5d0d13
pacotedemusicas <contato@pacotedemusicas.com>
2024-06-17 21:54:22 -0300

    Add files via upload

---
+++ pages/script3.js
@@ -0,0 +1,3 @@
+document.getElementById('button-caminhoneiro').addEventListener('click', function() {
+    window.open('https://drive.google.com/drive/folders/162fBQaSBNWBrJMnHQtO27THux4XvHFXY', '_blank');
+});
