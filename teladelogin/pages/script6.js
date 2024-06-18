bf1796217659d0b338d203ac8962d588d1cf84ad
pacotedemusicas <contato@pacotedemusicas.com>
2024-06-17 21:26:08 -0300

    Delete teladelogin directory

--- teladelogin/pages/script6.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('karaokeButton').addEventListener('click', function() {
-    window.open('https://drive.google.com/drive/folders/1br8WboYStVHII_N_YVR6NUx-tCWMAalD', '_blank');
-});
--- teladelogin/pages/script7.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('playbackButton').addEventListener('click', function() {
-    window.open('https://drive.google.com/drive/folders/11ziAQv2cjtNapdCW8qPk7dSiqUFFCMID', '_blank');
-});
