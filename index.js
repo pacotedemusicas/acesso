0121c6a40046bbfc34cd165d74be4c657d576764
pacotedemusicas <contato@pacotedemusicas.com>
2024-09-02 19:53:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -68,3 +68,16 @@ const form = {
     password: () => document.getElementById("password"),
     passwordRequiredError: () => document.getElementById("password-required-error"),
 };
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById("password");
+    const eyeIcon = document.getElementById("eye-icon");
+    
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.setAttribute("d", "M12 4.5c-4 0-7.5 2.5-9 6s-2.5 7.5 0 10 5.5 5 9 5 7.5-2.5 9-6 2.5-7.5 0-10-5.5-6-9-6zm0 10c-1.5 0-3-1.5-3-3s1.5-3 3-3 3 1.5 3 3-1.5 3-3 3z"); // Olho aberto
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.setAttribute("d", "M12 6.5c-2.75 0-5.5 1.5-7 4s-1.5 5.5 0 7 4.25 4 7 4 5.5-1.5 7-4 1.5-5.5 0-7-4.25-4-7-4zm0 6.5c-1.5 0-3-1.5-3-3s1.5-3 3-3 3 1.5 3 3-1.5 3-3 3zm0-8c-3.5 0-6.5 2-8 5s-2 6.5 0 9 4.5 5 8 5 6.5-2 8-5 2-6.5 0-9-4.5-5-8-5z"); // Olho fechado
+    }
+}
